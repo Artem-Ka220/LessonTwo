2d666f36d91f1dc03486630c7f34689eaa6b544b
arvsokolov <marktwe@mail.ru>
2023-05-21 14:05:33 +0300

    Added the tenth task.

---
+++ TaskOne/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+Console.WriteLine("Enter some number: ");
+int yourNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Second digit your number: {secondNumber(yourNumber)}.");
+
+int secondNumber(int num){
+    int firstNumber = num / 100 * 100;
+    int lastNumber = num % 10;
+    int secondNumber = (num - firstNumber - lastNumber) / 10;
+    return secondNumber;
+}
